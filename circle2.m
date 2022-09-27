dfc9a5c2e098c61c341312e478d2f72fb5542ffb
ZhengjieXiong2021 <91766170+ZhengjieXiong2021@users.noreply.github.com>
2022-09-27 18:51:16 -0400

    Add files via upload
    Annulus domian with inner radius 0.5 and outer radius 1

---
+++ circle2.m
@@ -0,0 +1,45 @@
+clearvars; close all;
+iseed = 8675309;
+rng(iseed);
+
+% discretize domain
+
+cparams = [];
+cparams.eps = 1.0e-10;
+cparams.nover = 0;
+cparams.maxchunklen = 0.1; % setting a chunk length helps when the
+                              % frequency is known
+                              
+pref = []; 
+chnkr1 = chunkerfunc(@(t) circle1(t,1),cparams,pref); 
+chnkr1 = chnkr1.refine;
+chnkr2 = chunkerfunc(@(t) circle1(t,0.5),cparams,pref); 
+chnkr2 = reverse(chnkr2);
+chnkr2 = chnkr2.refine;
+chnkrs = [chnkr1,chnkr2];
+chnkr = merge(chnkrs);
+chnkr = chnkr.refine();
+
+[~,~,info] = sortinfo(chnkr);
+assert(info.ier == 0);
+
+
+
+
+% plot geometry and data
+
+figure(1)
+clf
+plot(chnkr,'-x')
+hold on
+quiver(chnkr)
+axis equal
+
+
+
+% fkern = @(s,t) chnk.lap2d.kern(s,t,'D');
+% opts = [];
+% D = chunkermat(chnkr,fkern,opts);
+% sys = -0.5*eye(chnkr.npt) + D;
+% size(sys)
+
