f9f84614eb0b66bf772c5f803e6b649be1fdea16
ZhengjieXiong2021 <91766170+ZhengjieXiong2021@users.noreply.github.com>
2022-09-27 20:15:39 -0400

    Update annulustest2.m

--- annulustest2.m
+++ annulustest2.m
@@ -7,7 +7,8 @@ g = cos(3.2*(x+y).^2 +y.^2);
 n=50;
 k=1:n;
 x_cb=4.675+0.025*cos((2*k-1)/(2*n)*pi); %Chebyshev points
-rhs=zeros(size(x_cb));
+
+rhs=zeros(size(x_cb));                  %monitor function appliedd at Chebyshev points
 for i=1:n
     zk=x_cb(i);
     fkern = @(s,t) chnk.helm2d.kern(zk,s,t,'d');
@@ -17,10 +18,11 @@ for i=1:n
     rhs(i)=1/(g*(sys\f));
 end
 rhs=rhs';
-p=0:55; 
+
+p=0:55;      %Chebyshev polynomial evaluated at Chebyshev points, modified for interval [4.65,4.7]
 A=cos(x_cb'.*acos((p-4.675).*40));
 A=A';      
 A=A(1:50,1:50);
-a=A\rhs                        
-sol=colleagueeigs(49,a');
-sol=sol/40+4.675
+a=A\rhs      %coefficients of Chebyshev polynomials                  
+sol=colleagueeigs(49,a');  %eigenvalues of colleague matrix
+sol=sol/40+4.675 %transformation
